9f254714af611363c6ce1a11c1c6258e897575bf
Kori <koricrash@gamil.com>
2022-09-08 19:40:05 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,178 +1,195 @@
-/* 滑动监听 */
-window.addEventListener("scroll", function () {
-  const header = this.document.querySelector("header");
-  header.classList.toggle("sticky", this.window.scrollY > 0)
+/* navigation bar effects on scroll*/
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 0)
 })
 
-/* 服务遮罩层 */
-const serviceModals = document.querySelectorAll(".service-modal");
-const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
-const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
 
-var modal = function (modalClick) {
-  serviceModals[modalClick].classList.add("active");
+// 服务 --modal
+const serviceModals = document.querySelectorAll('.service-modal')
+const learnMoreBtns = document.querySelectorAll('.learn-more-btn')
+const modalCloseBtns = document.querySelectorAll('.modal-close-btn')
+
+function serviceModal(modalClickIndex) {
+  serviceModals[modalClickIndex].classList.add('active')
+}
+function serviceModalClose(modalClickIndex) {
+  serviceModals[modalClickIndex].classList.remove('active')
 }
 
-learnmoreBtns.forEach(function (learnmoreBtn, i) {
-  learnmoreBtn.addEventListener("click", () => {
-      modal(i);
-  });
-});
-modalCloseBtns.forEach((modalCloseBtn) => {
-  modalCloseBtn.addEventListener("click", () => {
-      serviceModals.forEach((modalView) => [
-          modalView.classList.remove("active")
-      ])
+learnMoreBtns.forEach((learnMoreBtn, index) => {
+  learnMoreBtn.addEventListener('click', () => {
+    serviceModal(index)
   })
 })
 
-/* 作品遮罩层 */
-const portfolioModals = document.querySelectorAll(".portfolio-model");
-const imgCards = document.querySelectorAll(".img-card");
-const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
-
-var portfolioModal = function (modalClick) {
-  portfolioModals[modalClick].classList.add("active");
-}
-
-imgCards.forEach(function (imgCard, i) {
-  imgCard.addEventListener("click", () => {
-      portfolioModal(i);
-  });
-});
-portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-  portfolioCloseBtn.addEventListener("click", () => {
-      portfolioModals.forEach((portfolioModalView) => {
-          portfolioModalView.classList.remove("active")
-      })
+modalCloseBtns.forEach((modalCloseBtn, index) => {
+  modalCloseBtn.addEventListener('click', () => {
+    serviceModalClose(index)
   })
 })
 
-/* 回到顶部按钮 */
-const scrollTopbtn = document.querySelector(".scrollToTop-btn")
-window.addEventListener("scroll", function () {
 
-  scrollTopbtn.classList.toggle("active", window.scrollY > 500)
+// 作品 --modal
+const worksModals = document.querySelectorAll('.works-modal')
+const imgCards = document.querySelectorAll('.img-card')
+const worksCloseBtns = document.querySelectorAll('.works-close-btn')
+
+function worksModal(modalClickIndex) {
+  worksModals[modalClickIndex].classList.add('active')
+}
+function worksModalClose(modalClickIndex) {
+  worksModals[modalClickIndex].classList.remove('active')
+}
+
+imgCards.forEach((imgCard, index) => {
+  imgCard.addEventListener('click', () => {
+    worksModal(index)
+  })
 })
 
-scrollTopbtn.addEventListener("click", () => {
+worksCloseBtns.forEach((worksCloseBtn, index) => {
+  worksCloseBtn.addEventListener('click', () => {
+    worksModalClose(index)
+  })
+})
+
+
+// 明暗切换
+const themeBtn = document.querySelector('.theme-btn')
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme')
+  themeBtn.classList.toggle('sun')
+
+  localStorage.setItem('saved-theme', getCurrentTheme())
+  localStorage.setItem('saved-icon', getCurrentIcon())
+})
+
+const getCurrentTheme = () => document.body.classList.contains('dark-theme') ? "dark" : "light"
+const getCurrentIcon = () => themeBtn.classList.contains('sun') ? "sun" : "moon"
+
+const savedTheme = localStorage.getItem('saved-theme')
+const savedIcon = localStorage.getItem('saved-icon')
+
+if (savedTheme) {
+  document.body.classList[savedTheme === 'dark' ? 'add' : 'remove']('dark-theme')
+  themeBtn.classList[savedIcon === 'sun' ? 'add' : 'remove']('sun')
+}
+
+
+
+
+
+// 返回顶部
+const scrollToTopBtn = document.querySelector('.scrollToTop')
+
+window.addEventListener('scroll', () => {
+  scrollToTopBtn.classList.toggle('active', window.scrollY > 500)
+})
+
+scrollToTopBtn.addEventListener('click', () => {
   document.body.scrollTop = 0
   document.documentElement.scrollTop = 0
 })
 
-/* 页面滚动时导航菜单项处于活动状态 */
-window.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll("section")
-  const scrollY = window.pageYOffset
 
-  sections.forEach(current => {
-      let sectionHeight = current.offsetHeight;
-      let sectionTop = current.offsetTop - 50;
-      let id = current.getAttribute("id")
 
-      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-          document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active")
-      } else {
-          document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active")
 
-      }
+// 导航栏菜单高亮
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section')
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(item => {
+    let sectionHeight = item.offsetHeight
+    let sectionTop = item.offsetTop - 70
+    let id = item.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-items a[href*=' + id + ']').classList.add('active')
+    } else {
+      document.querySelector('.nav-items a[href*=' + id + ']').classList.remove('active')
+    }
+
   })
 })
 
-/* 光暗模式切换 */
-const themeBtn = document.querySelector(".theme-btn")
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme")
-  themeBtn.classList.toggle("sun")
 
-  localStorage.setItem("saved-theme", getCurrentTheme())
-  localStorage.setItem("saved-icon", getCurrentIcon())
+
+// 关闭菜单
+
+const menuBtn = document.querySelector('.nav-menu-btn')
+const menuCloseBtn = document.querySelector('.nav-close-btn')
+const navigation = document.querySelector('.navigation')
+const naviItems = document.querySelectorAll('.nav-items a')
+
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.add('active')
 })
 
-const getCurrentTheme = () => {
-  document.body.classList.contains("dark-theme") ? "dark" : "light"
-}
-const getCurrentIcon = () => {
-  themeBtn.classList.contains("sun") ? "sun" : "moon"
-}
-
-const savedTheme = localStorage.getItem("saved-theme")
-const savedIcon = localStorage.getItem("saved-icon")
-
-if (savedTheme) {
-  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme")
-  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun")
-}
-
-/* 缩小导航控制 */
-const menuBtn = document.querySelector(".nav-menu-btn")
-const closeBtn = document.querySelector(".nav-close-btn")
-const navigation = document.querySelector(".navigation")
-const navItems = document.querySelectorAll(".nav-items a")
-
-menuBtn.addEventListener("click", () => {
-  navigation.classList.add("active")
+menuCloseBtn.addEventListener('click', () => {
+  navigation.classList.remove('active')
 })
 
-closeBtn.addEventListener("click", () => {
-  navigation.classList.remove("active")
-})
-
-navItems.forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-      navigation.classList.remove("active")
+naviItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navigation.classList.remove('active')
   })
 })
 
-/* 滑动动画 */
+
+// 发送消息
+let emitBtn = document.querySelector('.emitBtn')
+emitBtn.addEventListener('click', () => {
+  alert('功能未启用')
+})
+
+
+
+
+// 滚动动画
+
 ScrollReveal({
-  //reset: true,
+  // reset: true,
   distance: '60px',
-  duration: 2500,
-  delay: 100
+  duration: 2000,
 });
 
-ScrollReveal().reveal('.home .info h2,.section-title-01,.section-title-02', {
-  delay: 500,
-  origin: 'left'
-})
-ScrollReveal().reveal('.home .info h3,.home .info p,.about-info .btn', {
-  delay: 600,
-  origin: 'right'
-})
-ScrollReveal().reveal('.home .info .btn', {
-  delay: 700,
-  origin: 'bottom'
-})
-ScrollReveal().reveal('.media-icons i, .contact-left li', {
-  delay: 500,
-  origin: 'left',
-  interval: 200
-})
-ScrollReveal().reveal('.home-img, .about-img', {
-  delay: 500,
-  origin: 'bottom'
-})
-ScrollReveal().reveal('.about .description, .contact-right', {
-  delay: 600,
-  origin: 'right'
-})
-ScrollReveal().reveal('.about .professional-list li', {
-  delay: 500,
-  origin: 'right',
-  interval: 200
-})
-ScrollReveal().reveal('.get-in-touch .containter .content ,.skills-description ,.services-description ,.contact-card,.contact-left h2', {
-  delay: 700,
-  origin: 'left',
-})
-ScrollReveal().reveal('.experience-card ,.service-card ,.education,.portfolio .img-card', {
-  delay: 800,
-  origin: 'bottom',
-  interval:200
-})
-ScrollReveal().reveal('footer .group', {
-  delay: 800,
-  origin: 'top',
-  interval:200
-})
+
+
+// 定义动画
+
+
+
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 400, origin: 'left' });
+ScrollReveal().reveal('.home .info h3,.home .info p,.about-info .btn, .contact-right', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.home .info .btn', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.about .description', { delay: 400, origin: 'right' });
+
+
+
+
+ScrollReveal().reveal('.home .media-icons a, .contact-left li, .service-description>h3', { delay: 400, origin: 'left', interval: 150 });
+ScrollReveal().reveal('.profession-list .list-item', { delay: 500, origin: 'right', interval: 150 });
+ScrollReveal().reveal('.skills-description, service-description, .contact-card, .contact-left h2', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .works .img-card', { delay: 700, origin: 'bottom', interval: 150 });
+ScrollReveal().reveal('footer .group', { delay: 300, origin: 'top', interval: 150 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 刷新自动滚动到历史位置
+function showInfo(type, code) { window.location.href = "__URL__/show" + type + "/id/" + code; } function Trim(strValue) { return strValue.replace(/^s*|s*$/g, ""); } function SetCookie(sName, sValue) { document.cookie = sName + "=" + escape(sValue); } function GetCookie(sName) { var aCookie = document.cookie.split(";"); for (var i = 0; i < aCookie.length; i++) { var aCrumb = aCookie[i].split("="); if (sName == Trim(aCrumb[0])) { return unescape(aCrumb[1]); } } return null; } function scrollback() { if (GetCookie("scroll") != null) { document.body.scrollTop = GetCookie("scroll") } }
---
+++ readme.md
@@ -0,0 +1,19 @@
+## 个人网站
+
+### 技术栈：html + css + javascript
+
+
+
+##### 字体来源  Google-fonts 
+
+##### 字体样式  poppins所有样式，使用@import导入 https://fonts.google.com/specimen/Poppins?query=poppins
+
+##### 图标库  font awesome  https://fontawesome.com/search?m=free&o=r
+
+##### 滚动动画库  ScrollReveal https://scrollrevealjs.org/  
+
+
+
+
+
+-- kori
